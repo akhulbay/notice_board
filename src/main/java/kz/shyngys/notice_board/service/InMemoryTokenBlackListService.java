58571c7e0db8739d285bd57a71f43c8cb06d6af5
sakhulbay <sakhulbay@greet-go.com>
2024-07-21 23:18:24 +0500

    feat: added todo

--- src/main/java/kz/shyngys/notice_board/service/InMemoryTokenBlackListService.java
+++ src/main/java/kz/shyngys/notice_board/service/InMemoryTokenBlackListService.java
@@ -1,5 +1,6 @@
 package kz.shyngys.notice_board.service;
 
+// todo shyngys replace with better solution, so api have to be STATELESS
 public interface InMemoryTokenBlackListService {
 
     void addToBlacklist(String token);
