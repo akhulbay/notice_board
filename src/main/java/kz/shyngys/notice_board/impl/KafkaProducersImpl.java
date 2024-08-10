e598678e6f525cd801d3453ca1059c3c3af91699
sakhulbay <sakhulbay@greet-go.com>
2024-08-10 20:15:57 +0500

    feat: some code fixes

--- src/main/java/kz/shyngys/notice_board/impl/KafkaProducersImpl.java
+++ src/main/java/kz/shyngys/notice_board/impl/KafkaProducersImpl.java
@@ -4,7 +4,6 @@ import kz.shyngys.notice_board.model.email.Email;
 import kz.shyngys.notice_board.model.kafka.KafkaTopic;
 import kz.shyngys.notice_board.service.KafkaProducers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +11,11 @@ import org.springframework.stereotype.Component;
 public class KafkaProducersImpl implements KafkaProducers {
 
     @Autowired
-    @Qualifier("emailKafkaTemplate")
-    private KafkaTemplate<String, Email> kafkaTemplate;
+    private KafkaTemplate<String, Email> emailKafkaTemplate;
 
     @Override
     public void sendEmail(Email email) {
-        kafkaTemplate.send(KafkaTopic.EMAIL.name(), email);
+        emailKafkaTemplate.send(KafkaTopic.EMAIL.name(), email);
     }
 
 
