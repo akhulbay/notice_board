b012b60918257a9e927d4ab209c905a9edd41c43
sakhulbay <sakhulbay@greet-go.com>
2024-08-11 17:15:19 +0500

    fix: removed todo

--- src/main/java/kz/shyngys/notice_board/service/TokenBlackListService.java
+++ src/main/java/kz/shyngys/notice_board/service/TokenBlackListService.java
@@ -1,6 +1,5 @@
 package kz.shyngys.notice_board.service;
 
-// todo shyngys replace with better solution, so api have to be STATELESS, can extract to Postgres table
 public interface TokenBlackListService {
 
     void addToBlacklist(String token);
